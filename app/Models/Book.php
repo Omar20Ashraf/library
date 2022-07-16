7cd3d52ee68afa4ca96ea16f15c87512c22e575d
Omar20Ashraf <omaarashraf2020@gmail.com>
2022-07-16 17:22:31 +0200

    book checkout and check in

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -20,4 +20,32 @@ class Book extends Model
             'name' => $author
         ])->id;
     }
+
+    public function reservations()
+    {
+        # code...
+        return $this->hasMany(Reservation::class,'book_id');
+    }
+
+    public function checkout($user)
+    {
+        # code...
+
+        $this->reservations()->create([
+            'user_id' => $user->id,
+            'checked_out_at' => now(),
+        ]);
+    }
+
+    public function checkin($user)
+    {
+        # code...
+        $reservation =  $this->reservations()
+                            ->where('user_id',$user->id)
+                            ->whereNotNull('checked_out_at')
+                            ->whereNull('checked_in_at')
+                            ->first();
+
+        $reservation->update(['checked_in_at' => now()]);
+    }
 }
---
+++ app/Models/Reservation.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Reservation extends Model
+{
+    use HasFactory;
+
+    protected $table = 'reservations';
+
+    protected $fillable = [
+        'user_id',
+        'book_id',
+        'checked_out_at',
+        'checked_in_at',
+    ];
+}
